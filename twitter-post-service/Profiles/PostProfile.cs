de5bf6f578ebfe4fbc343f7e0eac9b5b13d0f4d6
NickVanGerwen <nick.gerwen@outlook.com>
2023-04-04 11:42:21 +0200

    add autoMapper

---
+++ twitter-post-service/Profiles/PostProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace twitter_post_service.Profiles
+{
+    public class PostProfile : Profile
+    {
+        public PostProfile()
+        {
+            CreateMap<Models.Post, DTOs.PostReadDTO>();
+            CreateMap<DTOs.PostCreateDTO, Models.Post>();
+        }
+    }
+}
