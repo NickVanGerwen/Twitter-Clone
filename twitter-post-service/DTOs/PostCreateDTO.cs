e32fa990902656a8dc6054151401faa92a474f3f
NickVanGerwen <nick.gerwen@outlook.com>
2023-04-04 14:29:16 +0200

    update dtos

--- twitter-post-service/DTOs/PostCreateDTO.cs
+++ twitter-post-service/DTOs/PostCreateDTO.cs
@@ -2,5 +2,10 @@
 {
     public class PostCreateDTO
     {
+        public string Message { get; set; }
+        public string Author { get; set; }
+        public DateTime Date { get; set; }
+        public int Likes { get; set; }
     }
 }
+
--- twitter-post-service/DTOs/PostReadDTO.cs
+++ twitter-post-service/DTOs/PostReadDTO.cs
@@ -2,5 +2,10 @@
 {
     public class PostReadDTO
     {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public string Author { get; set; }
+        public DateTime Date { get; set; }
+        public int Likes { get; set; }
     }
 }
