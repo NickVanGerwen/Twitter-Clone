95e83d075c009d4c5e8390af91a350de54a36d82
NickVanGerwen <nick.gerwen@outlook.com>
2023-05-09 13:47:44 +0200

    Update Program.cs

--- twitter-fetch-service/Program.cs
+++ twitter-fetch-service/Program.cs
@@ -4,8 +4,6 @@ using twitter_post_service.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
